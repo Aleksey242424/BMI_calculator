ef56eebe6382376857f33ef1a95f83e27f1e4146
Aleksey242424 <alekseysokolovsky_256@mail.ru>
2026-03-26 02:35:51 +0300

    add new functional

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 )
 
 func main() {
-	var userHeight, userWeight float64 = 1.8, 100
-	BMI := userWeight / math.Pow(userHeight, 2)
-	fmt.Print(BMI)
+	const BMIPower = 2
+	var userHeight, userWeight float64
+	fmt.Print("____ Калькулятор ИМТ ____\n\n")
+	fmt.Print("Введите свой рост в метрах: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес: ")
+	fmt.Scan(&userWeight)
+	BMI := userWeight / math.Pow(userHeight, BMIPower)
+	fmt.Print("Ваш ИМТ: ", BMI)
 }
