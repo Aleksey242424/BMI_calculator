9749af7c82407402773f826fcddc27e25aa65e42
Aleksey242424 <alekseysokolovsky_256@mail.ru>
2026-03-26 02:51:02 +0300

    fix code

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 func main() {
 	const BMIPower = 2
 	var userHeight, userWeight float64
-	fmt.Print("____ Калькулятор ИМТ ____\n\n")
-	fmt.Print("Введите свой рост в метрах: ")
+	fmt.Println("____ Калькулятор ИМТ ____")
+	fmt.Print("Введите свой рост в сантиметрах: ")
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите свой вес: ")
 	fmt.Scan(&userWeight)
-	BMI := userWeight / math.Pow(userHeight, BMIPower)
-	fmt.Print("Ваш ИМТ: ", BMI)
+	BMI := userWeight / math.Pow(userHeight/100, BMIPower)
+	fmt.Printf("Ваш ИМТ: %.2f", BMI)
 }
