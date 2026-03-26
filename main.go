c30067411761a972bde714d65c3d323f9107ab44
Aleksey242424 <alekseysokolovsky_256@mail.ru>
2026-03-26 03:16:53 +0300

    add func calculate

--- main.go
+++ main.go
@@ -6,13 +6,18 @@ import (
 )
 
 func main() {
-	const BMIPower = 2
 	var userHeight, userWeight float64
 	fmt.Println("____ Калькулятор ИМТ ____")
 	fmt.Print("Введите свой рост в сантиметрах: ")
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите свой вес: ")
 	fmt.Scan(&userWeight)
-	BMI := userWeight / math.Pow(userHeight/100, BMIPower)
+	BMI := calculateIMT(userHeight, userWeight)
 	fmt.Printf("Ваш ИМТ: %.2f", BMI)
+}
+
+func calculateIMT(userHeight, userWeight float64) float64 {
+	const BMIPower = 2
+	result := userWeight / math.Pow(userHeight/100, BMIPower)
+	return result
 }
