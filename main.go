1e24e6276253a56ebe77903bc0f914dc25508721
Aleksey242424 <alekseysokolovsky_256@mail.ru>
2026-03-26 03:23:40 +0300

    add func for get input

--- main.go
+++ main.go
@@ -6,12 +6,8 @@ import (
 )
 
 func main() {
-	var userHeight, userWeight float64
 	fmt.Println("____ Калькулятор ИМТ ____")
-	fmt.Print("Введите свой рост в сантиметрах: ")
-	fmt.Scan(&userHeight)
-	fmt.Print("Введите свой вес: ")
-	fmt.Scan(&userWeight)
+	var userHeight, userWeight float64 = getUserInput()
 	BMI := calculateIMT(userHeight, userWeight)
 	fmt.Printf("Ваш ИМТ: %.2f", BMI)
 }
@@ -20,4 +16,13 @@ func calculateIMT(userHeight, userWeight float64) float64 {
 	const BMIPower = 2
 	result := userWeight / math.Pow(userHeight/100, BMIPower)
 	return result
+}
+
+func getUserInput() (float64, float64) {
+	var userHeight, userWeight float64
+	fmt.Print("Введите свой рост в сантиметрах: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес: ")
+	fmt.Scan(&userWeight)
+	return userHeight, userWeight
 }
