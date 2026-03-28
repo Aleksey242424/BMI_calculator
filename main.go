78554d08afaea6c0193f0d6c1b7b780fa1b135ed
Aleksey242424 <alekseysokolovsky_256@mail.ru>
2026-03-28 08:16:11 +0300

    add status

--- main.go
+++ main.go
@@ -5,15 +5,32 @@ import (
 	"math"
 )
 
+const BMIPower = 2
+
 func main() {
 	fmt.Println("____ Калькулятор ИМТ ____")
 	var userHeight, userWeight float64 = getUserInput()
 	BMI := calculateIMT(userHeight, userWeight)
-	fmt.Printf("Ваш ИМТ: %.2f", BMI)
+	status := ""
+	if BMI < 16 {
+		status = "Сильный дефицит массы тела !!!"
+	} else if BMI > 16 && BMI <= 18.5 {
+		status = "Дефицит массы тела !!"
+	} else if BMI > 18.5 && BMI <= 25 {
+		status = "Норма."
+	} else if BMI > 25 && BMI <= 30 {
+		status = "Избыточная масса !"
+	} else if BMI > 30 && BMI <= 35 {
+		status = "1-я степень ожирения !!"
+	} else if BMI > 35 && BMI <= 40 {
+		status = "2-я степень ожирения !!"
+	} else {
+		status = "3-я степень ожирения !!!!"
+	}
+	fmt.Printf("Ваш ИМТ: %.2f\nСтатус: %v", BMI, status)
 }
 
 func calculateIMT(userHeight, userWeight float64) float64 {
-	const BMIPower = 2
 	result := userWeight / math.Pow(userHeight/100, BMIPower)
 	return result
 }
